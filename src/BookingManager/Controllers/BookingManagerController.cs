5cc3db340e75cebf9f1200b94d65be2e17b3784c
nigel wareham <nigel.wareham@freightways.co.nz>
2017-09-12 23:50:58 +0000

    -//-

---
+++ src/BookingManager/Controllers/BookingManagerController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Bookings.Models;
+
+namespace BookingManager.Controllers
+{
+    [Route("api/BookingManager")]
+    public class BookingManagerController
+    {
+        // POST api/values
+        [HttpPost]
+        public String Post([FromBody] Booking bookingMessage)
+        {
+            return Guid.NewGuid().ToString();
+        }
+
+
+    }
+}
